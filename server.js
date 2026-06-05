8776b9f56522d1a0ad9630c8ff75aa44fb30d73a
Kuru <90812407+nnnnni02@users.noreply.github.com>
2026-06-05 21:02:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,12 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  family: 4
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false }
 });
 
 const app = express();
